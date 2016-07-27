69ec978d188791ad9be6b3e31b0918f6cfc96337
Hendro Wicaksono <hendrowicaksono@gmail.com>
2016-07-28 05:51:23 +0700

    Update src\Bibliography\Collection

--- src/Bibliography/Collection.php
+++ src/Bibliography/Collection.php
@@ -3,17 +3,36 @@ namespace Slims\Bibliography;
 
 class Collection
 {
+  public $is_newcoll = TRUE;
   public $coll = array();
 
   public function __construct()
   {
     #$this->coll['title'] = NULL;
-    echo 'Slims\Bibliography\Collection()';
+    #echo 'Slims\Bibliography\Collection()';
   }
 
   protected function newCollection()
   {
     $this->setTitle();
+  }
+
+  protected function setIs_newcoll($cid)
+  {
+    if (!is_null($cid)) {
+      if (is_numeric($cid)) {
+        $this->is_newcoll = FALSE;
+      } else {
+        $this->is_newcoll = TRUE;
+      }
+    } else {
+      $this->is_newcoll = TRUE;
+    }
+  }
+
+  protected function getIs_newcoll()
+  {
+    return $this->is_newcoll;
   }
 
   protected function setTitle($title=NULL)
@@ -23,9 +42,13 @@ class Collection
 
   public function collection_load($cid = NULL)
   {
-    if (is_null($cid)) {
-      $this->newCollection();
-      return $this->coll;
+    $this->setIs_newcoll($cid);
+    #$this->setTitle($cid);
+    $this->getIs_newcoll();
+    if ($this->getIs_newcoll()) {
+      echo 'Buku baru';
+    } else {
+      echo 'Edit Buku';
     }
   }
 

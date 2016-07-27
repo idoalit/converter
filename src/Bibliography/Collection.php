48a5a4473381cfe3a505112273162ec7855171ce
hendrowicaksono <hendrowicaksono@yahoo.com>
2016-07-27 16:34:16 +0700

    Second commit

--- src/Bibliography/Collection.php
+++ src/Bibliography/Collection.php
@@ -7,13 +7,24 @@ class Collection
 
   public function __construct()
   {
-    $this->coll['title'] = NULL;
+    #$this->coll['title'] = NULL;
     echo 'Slims\Bibliography\Collection()';
+  }
+
+  protected function newCollection()
+  {
+    $this->setTitle();
+  }
+
+  protected function setTitle($title=NULL)
+  {
+    $this->coll['title'] = $title;
   }
 
   public function collection_load($cid = NULL)
   {
     if (is_null($cid)) {
+      $this->newCollection();
       return $this->coll;
     }
   }
